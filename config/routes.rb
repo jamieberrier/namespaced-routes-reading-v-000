0f9d1301829c817af319f6aab8d0fb686e324501
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-02 01:11:30 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :stats, only: [:index]
   end
-
+  # There is one important difference between scope '/admin', module: 'admin' and namespace :admin, and it's in the URL helpers. Remember above that using scope gave us a stats_path helper. But now that we are using namespace, run rake routes again. You'll see that the helper is now prefixed with admin_, so stats_path becomes admin_stats_path. If you switch from scope to namespace, take care to update any URL helpers you have in use!
   root 'posts#index'
 end
